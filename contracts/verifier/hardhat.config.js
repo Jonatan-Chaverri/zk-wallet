a054122af4cf9e29b42c2c582b78e2d59dbe7986
Jonatan Chaverri <jonathan.chaverri12@gmail.com>
2025-11-07 18:54:28 -0300

    Remove user wallet, add verifier

---
+++ contracts/verifier/hardhat.config.js
@@ -0,0 +1,36 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: {
+    version: "0.8.27",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    sepolia: {
+      url: process.env.RPC_URL || "https://sepolia-rollup.arbitrum.io/rpc",
+      accounts: process.env.ACCOUNT_PRIVATE_KEY ? [process.env.ACCOUNT_PRIVATE_KEY] : [],
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY || "",
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+};
+
